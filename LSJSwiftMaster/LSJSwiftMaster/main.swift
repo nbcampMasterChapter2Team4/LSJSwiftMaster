e61f4c30fce1b66795d0141f5f3453a282f18fb0
Yimkeul <leesjun29@gmail.com>
2025-03-17 11:53:36 +0900

    feat: Lv1

--- LSJSwiftMaster/LSJSwiftMaster/main.swift
+++ LSJSwiftMaster/LSJSwiftMaster/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+let sum: (Int,Int) -> String = { a, b in
+    return "두 수의 합은 \(a + b)입니다"
+}
 
+print(sum(1, 2))
+
+
+func calculate(sumClousre: (Int,Int) -> String) -> Void {
+    print(sum(3,4))
+}
+
+calculate(sumClousre: sum)
