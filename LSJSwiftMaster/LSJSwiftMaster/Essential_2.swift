f134f532fae84116f1512771d9de65c11656f733
Yimkeul <leesjun29@gmail.com>
2025-03-17 12:11:16 +0900

    feat: 필수 문제 2

--- LSJSwiftMaster/LSJSwiftMaster/Essential_2.swift
+++ LSJSwiftMaster/LSJSwiftMaster/Essential_2.swift
@@ -8,5 +8,32 @@
 import Foundation
 
 func Essential_2() {
+    let numbers = [1, 2, 3, 4, 5]
+    var result = [String]()
+
+    func Q1() {
+        result = numbers.map { String($0) }
+        print(result)
+    }
+    Q1()
     
+    
+    func Q2(for input: [Int]) -> [String] {
+        return input.filter { $0 % 2 == 0 }.map { String($0) }
+    }
+    
+    let sampleInputForQ2: [Int] = [1,2,3,4,5,6,7,8,9,10]
+    
+    print(Q2(for: sampleInputForQ2), type(of: Q2(for: sampleInputForQ2)))
+
+    func Q3() {
+        func myMap(_ array: [Int], _ changer: (Int) -> String) -> [String] {
+            return array.map { changer($0) }
+        }
+        let result = myMap([1, 2, 3, 4, 5]) {
+            String($0)
+        }
+        print(result)
+    }
+    Q3()
 }
